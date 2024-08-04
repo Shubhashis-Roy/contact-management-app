f20c51d0b728583ae35f2543ff2fd9dae0668b9c
Shubhashis Roy <shubhashisroy360@gmail.com>
2024-08-04 21:30:51 +0530

    feat: side bar UI

--- src/component/Sidebar.tsx
+++ src/component/Sidebar.tsx
@@ -1,17 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { MdOutlineContactMail } from "react-icons/md";
+import { IoBarChartSharp } from "react-icons/io5";
+import { FaMapMarkerAlt } from "react-icons/fa";
 
 const Sidebar = () => {
   return (
-    <div className=" w-[75px] mt-[66px] h-full fixed bg-gray-300">
-      <div className="ml-[7px] h-[70px] pt-3 hover:bg-gray-300 duration-200 rounded-lg cursor-pointer">
+    <div className=" w-[80px] h-full fixed bg-gray-200">
+      <div className="ml-[7px] mr-[7px] mt-[40px] h-[70px] pt-3 hover:bg-gray-300 duration-200 rounded-lg cursor-pointer">
         <Link to="/">
-          <p className="text-[12px] ml-[16px]">Contact</p>
+          <MdOutlineContactMail className="text-[28px] ml-[16px]" />
+          <p className="text-[16px] ml-[1px]">Contact</p>
         </Link>
       </div>
-      <div className="ml-[7px] h-[70px] pt-3 hover:bg-gray-300 duration-200 rounded-lg cursor-pointer">
+      <div className="ml-[7px] mr-[7px] mt-[40px] h-[70px] pt-3 hover:bg-gray-300 duration-200 rounded-lg cursor-pointer">
         <Link to="/chart">
-          <p className="text-[12px] ml-[16px]">Chart</p>
+          <IoBarChartSharp className="text-[28px] ml-[16px]" />
+          <p className="text-[16px] ml-[8px]">Chart</p>
+        </Link>
+      </div>
+      <div className="ml-[7px] mr-[7px] mt-[40px] h-[70px] pt-3 hover:bg-gray-300 duration-200 rounded-lg cursor-pointer">
+        <Link to="/map">
+          <FaMapMarkerAlt className="text-[28px] ml-[16px]" />
+          <p className="text-[16px] ml-[12px]">Map</p>
         </Link>
       </div>
     </div>
